810991e5955080c748a2314a0ec41080785ab311
paololoddo <paolo.loddo@gmail.com>
2020-04-13 13:15:21 +0200

    product detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,7 +23,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
   ],
   bootstrap: [ AppComponent ]
 })
