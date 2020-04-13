d8f320217ba5bd5ea5120fe643f5cb53bae07d18
paololoddo <paolo.loddo@gmail.com>
2020-04-13 16:27:43 +0200

    shipping component

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class CartService {
   items = [];
+
+  constructor(
+    private http: HttpClient
+  ) {}
 
   addToCart(product) {
     this.items.push(product);
@@ -15,5 +20,9 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
   }
 }
